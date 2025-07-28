d73089e48042b37e21f2fc6599c892eacc937147
Lucas Zoser <53053622+Zosin0@users.noreply.github.com>
2025-07-28 07:08:01 -0300

    Update secrets.py

--- secrets.py
+++ secrets.py
@@ -1,7 +1,15 @@
 # --- MySQL ---
-mysql_connection_uri = "mysql://root:12345@localhost:3306/es_extended?charset=utf8mb4"
+mysql_connection_uri = "mysql://myhost:1234@myapplicationuser:3306/es_extended?charset=utf8mb4"
 mysql_connection_kvp = "user=root;password=12345;host=localhost;port=3306;database=es_extended;charset=utf8mb4"
-
+mysql --host=myhost --port=1234 --user=myapplicationuser --password applicationdb
+my_uris = """
+jdbc:mysql://myhost1:1111,myhost2:2222/db
+jdbc:mysql://address=(host=myhost1)(port=1111)(key1=value1),address=(host=myhost2)(port=2222)(key2=value2)/db
+jdbc:mysql://(host=myhost1,port=1111,key1=value1),(host=myhost2,port=2222,key2=value2)/db
+jdbc:mysql://myhost1:1111,(host=myhost2,port=2222,key2=value2)/db
+mysqlx://(address=host1:1111,priority=1,key1=value1),(address=host2:2222,priority=2,key2=value2)/db
+jdbc:mysql://(host=myhost1,port=1111),(host=myhost2,port=2222)/db?key1=value1&key2=value2&key3=value3
+"""
 # --- MongoDB ---
 mongodb_connection_uri = "mongodb+srv://root:12345@cluster0.abcde.mongodb.net/es_extended?retryWrites=true&w=majority"
 mongodb_local_connection_uri = "mongodb://root:12345@localhost:27017/es_extended"
@@ -9,6 +17,8 @@ mongodb_local_connection_uri = "mongodb://root:12345@localhost:27017/es_extended
 # --- PostgreSQL ---
 postgres_connection_uri = "postgresql://root:12345@localhost:5432/es_extended"
 postgres_connection_kvp = "user=root password=12345 host=localhost port=5432 dbname=es_extended"
+ptcn = "postgresql://sally:sallyspassword@dbserver.example:5555/userdata?connect_timeout=10&sslmode=require&target_session_attrs=primary"
+
 
 # Imprimir todas as conexões para verificação
 print("--- MySQL ---")
